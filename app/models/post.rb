a1fbf1e15700f4766fcd202020cf34fc768a2b1a
Oliver Pacut <oliver.pacut@gmail.com>
2016-04-09 00:31:24 +0200

    Changes to be committed: 	modified:   app/models/post.rb 	modified:   app/models/post.rb~ 	modified:   app/models/profile.rb 	modified:   app/models/profile.rb~

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  validates :content, length: { minimum: 1 }
+  validates :content, length: { minimum: 1 }, presence: true
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,5 @@
 class Profile < ActiveRecord::Base
   has_many :posts
+  validates email, presence: true
+  validates name, presence: true
 end
