ae8ac3330448ff4f4e6879c9c221d6f42887badd
Oliver Pacut <oliver.pacut@gmail.com>
2016-04-18 01:41:08 +0200

    User model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,11 @@
 class Profile < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   has_many :posts
-  validates :email, presence: true
-  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+		    format: { with: VALID_EMAIL_REGEX },
+		    uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: { maximum: 20 }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -1,6 +1,58 @@
 require 'test_helper'
 
 class ProfileTest < ActiveSupport::TestCase
+  def setup
+    @profile = Profile.new(name: "Jane Doe", email: "bestJane@example.com",
+			   password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @profile.valid?
+  end
+
+  test "name should be present" do
+    @profile.name = "     "
+    assert_not @profile.valid?
+  end
+
+  test "email should be present" do
+    @profile.email = "     "
+    assert_not @profile.valid?
+  end
+
+  test "name should not be too long" do
+    @profile.name = "a"*21
+    assert_not @profile.valid?
+  end
+
+  test "email should not be too long" do
+    @profile.email = "a"*244 + "@example.com"
+    assert_not @profile.valid?
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @profile.email = invalid_address
+      assert_not @profile.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @profile.dup
+    duplicate_user.email = @profile.email.upcase
+    @profile.save
+    assert_not duplicate_user.valid?
+  end
+  test "password should be present (nonblank)" do
+    @profile.password = @profile.password_confirmation = " " * 6
+    assert_not @profile.valid?
+  end
+
+  test "password should have a minimum length" do
+    @profile.password = @profile.password_confirmation = "a" * 5
+    assert_not @profile.valid?
+  end
   # test "the truth" do
   #   assert true
   # end
